204e9c9565bebfc41ee7a8512dfa78929bde7a14
Cix8 <francesco.natal_fn@libero.it>
2022-06-24 14:40:04 +0200

    project_scaffolding; add home, add partials/header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,47 @@
+<header class="header">
+    <div class="header_contents">
+        <div class="logo-container">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="">
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="">Characters</a>
+                </li>
+                <li>
+                    <a href="">Characters</a>
+                </li>
+                <li>
+                    <a href="">Characters</a>
+                </li>
+                <li>
+                    <a href="">Characters</a>
+                </li>
+                <li>
+                    <a href="">Characters</a>
+                </li>
+                <li>
+                    <a href="">Characters</a>
+                </li>
+                <li>
+                    <a href="">Characters</a>
+                </li>
+                <li>
+                    <a href="">Characters</a>
+                </li>
+                <li>
+                    <a href="">Characters</a>
+                </li>
+                <li>
+                    <a href="">Characters</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="search-container">
+            <input type="search" name="search" id="search">
+            <label for="search">
+                <i class="fas fa-search"></i>
+            </label>
+        </div>
+    </div>
+</header>
