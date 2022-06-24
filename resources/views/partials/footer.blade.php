82bfb120b518a04a8887e9fab41e9e2ec0ef5d9c
Cix8 <francesco.natal_fn@libero.it>
2022-06-24 18:17:55 +0200

    add contents in footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,55 +1,68 @@
 <footer>
     <div class="footer_top">
-        <div class="left">
-            <div class="col">
-                <ul>
-                    <li>
-                        <h3>DC COMICS</h3>
-                    </li>
-                    @foreach ($footer_links['dc_comics'] as $link)
-                        <li>
-                            <a href="">
-                                {{ $link }}
-                            </a>
+        <div class="container">
+            <div class="left">
+                <div class="col">
+                    <ul>
+                        <li class="title">
+                            <h3>DC COMICS</h3>
                         </li>
-                    @endforeach
-                </ul>
-                <ul>
-                    <li>
-                        <h3>SHOP</h3>
-                    </li>
-                    @foreach ($footer_links['shop'] as $link)
-                        <li>
-                            <a href="">{{ $link }}</a>
+                        @foreach ($footer_links['dc_comics'] as $link)
+                            <li class="link">
+                                <a href="">
+                                    {{ $link }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                    <ul>
+                        <li class="title">
+                            <h3>SHOP</h3>
                         </li>
-                    @endforeach
-                </ul>
+                        @foreach ($footer_links['shop'] as $link)
+                            <li class="link">
+                                <a href="">{{ $link }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="col">
+                    <ul>
+                        <li class="title">
+                            <h3>DC</h3>
+                        </li>
+                        @foreach ($footer_links['dc'] as $link)
+                            <li class="link">
+                                <a href="">{{ $link }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="col">
+                    <ul>
+                        <li class="title">
+                            <h3>SITES</h3>
+                        </li>
+                        @foreach ($footer_links['sites'] as $link)
+                            <li class="link">
+                                <a href="">{{ $link }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
             </div>
-            <div class="col">
-                <ul>
-                    <li>
-                        <h3>DC</h3>
-                    </li>
-                    @foreach ($footer_links['dc'] as $link)
-                        <li>
-                            <a href="">{{ $link }}</a>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-            <div class="col">
-                <ul>
-                    <li>
-                        <h3>SITES</h3>
-                    </li>
-                    @foreach ($footer_links['sites'] as $link)
-                        <li>
-                            <a href="">{{ $link }}</a>
-                        </li>
-                    @endforeach
-                </ul>
+            <div class="right">
+                <div class="img-container">
+                    <img src="{{ asset('../images/dc-logo-bg.png') }}" alt="dc logo">
+                </div>
             </div>
         </div>
+        <div class="credits">
+            <p>All Site Contents TM and 2020 DC Entertainment, unless otherwise <a href="">nothed here</a>.All rights reserved</p>
+            <a href="">Cookies Settings</a>
+        </div>
     </div>
-    <div class="footer_bottom"></div>
+    <div class="footer_bottom">
+
+    </div>
 </footer>
